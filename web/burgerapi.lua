24776b90fa308cefc7a1d2b84be44bef6c20b053
root <root@ns323982.ip-91-121-4.eu>
2016-04-09 01:41:45 +0200

    fixed dodgy burgerapi bbreaking things added notes to burgerapi for adding burgerposts

--- web/burgerapi.lua
+++ web/burgerapi.lua
@@ -2,17 +2,50 @@
 
 local redis = require 'resty.redis'
 
-local red = redis:new()
-local ok, err = red:connect("redis", 6379)
-if not ok then
-    ngx.say("failed to connect: ", err)
-    return
-end
 
 local api = {}
 
 function api:GetRedisConnection()
 
+  local red = redis:new()
+	local ok, err = red:connect("redis", 6379)
+	if not ok then
+	    ngx.say("failed to connect: ", err)
+	    return
+	end
+  return red
+end
+
+function api:CreateBurgerPost(burgerInfo)
+  --[[
+      hash:
+      burgerID ->
+        burgerID
+        burgerName
+        restaurantName
+        meatRating
+        bunRating
+        toppingRating
+        chipRating
+        notes
+
+      sets:
+      burgername -> burgerID
+      burgerID ->burgerName
+
+      sorted set
+      totalScore -> burgerID
+      date - > burgerID
+
+      geo:
+      add  burgerID: lat: long
+
+      What we want to show users:
+      top 10 burgers
+      nearest burgers
+      newest burgers
+
+    ]]
 end
 
 
