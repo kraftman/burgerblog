3624f3f947e4ea55943961258634bee44a66ddfb
Chris Tanner <chris.tanner@sslgb.com>
2017-03-29 16:47:06 +0100

    move map methods to class

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,111 +1,122 @@
 
 var MapHandler = function(map,markers) {
-  this.lat = 51.6660128
-  this.long = -0.4790519999999999
-  this.map = map
-  this.markers = markers
-}
+  this.lat = 51.6660128;
+  this.long = -0.4790519999999999;
+  this.markers = markers;
+  this.map = undefined;
+};
 
 MapHandler.prototype = function() {
 
-  return {
+  var showPosition = function(position){
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log(this.map);
+    console.log(this);
 
+    this.map.panTo(new google.maps.LatLng(parseFloat(lat), parseFloat(long)));
+    this.map.setZoom(10);
+  },
+  updateMarkers = function() {
+    $.getJSON('/api/nearest/'+this.lat+'/'+this.long, function(data){
+      $.each( data, function( key, burgerInfo ) {
+
+        var pinIcon = new google.maps.MarkerImage(
+          '/static/images/burger-fallback.png',
+          null, /* size is determined at runtime */
+          null, /* origin is 0,0 */
+          null, /* anchor is bottom center of the scaled image */
+          new google.maps.Size(20, 20)
+        );
+        var marker = new google.maps.Marker({map: this.map, icon: pinIcon});
+        var newLatLng = new google.maps.LatLng(parseFloat(burgerInfo.lat), parseFloat(burgerInfo.long));
+        //console.log(newLatLng)
+
+        marker.setPosition(newLatLng);
+        //marker.setLabel(burgerInfo.burgerName)
+      });
+    });
+  },
+  initMiniMap = function() {
+    var myLatlng = {lat: this.lat, lng: this.long};
+    var mapDiv = document.getElementById('minimap');
+    this.map = new google.maps.Map(mapDiv, {
+      center: {lat: this.lat, lng: this.long},
+      zoom: 8
+    });
+
+    updateMarkers();
+    google.maps.event.addListener(this.map, 'dragend',  updateMarkers );
+    google.maps.event.addListener(this.map, 'zoom_changed',  updateMarkers );
+  },
+  load = function() {
+    if (navigator.geolocation) {
+      var context = this;
+      navigator.geolocation.getCurrentPosition(function(position) {showPosition.call(context, position);});
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+      console.log('not supported');
+    }
+  },
+  moveTo = function(lat,long){
+    var myLatLong;
+    if (long === 'undefined') {
+      myLatLong = lat;
+    } else {
+      myLatLong = new google.maps.LatLng(parseFloat(lat), parseFloat(long));
+    }
+
+
+    this.map.panTo(myLatLong);
+    updateMarkers();
+
+  };
+
+  return {
+    showPosition: showPosition,
+    load: load,
+    initMiniMap: initMiniMap,
+    moveTo: moveTo
   };
 }();
 
 
-var lat = 51.6660128
-var long = -0.4790519999999999
-var map
-var markers = [];
-
-if (navigator.geolocation) {
-  console.log('loc available')
-  navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-  x.innerHTML = "Geolocation is not supported by this browser.";
-  console.log('not supported')
+function InitMap(){
+  myMapHandler.addMap();
+  myMapHandler.checkMap();
 }
 
-function showPosition(position) {
-   lat = position.coords.latitude
-   long = position.coords.longitude
-   console.log(lat, long)
-   map.panTo(new google.maps.LatLng(parseFloat(lat), parseFloat(long)))
-   map.setZoom(10)
-}
+
+var myMapHandler = new MapHandler();
+
 
 $(function() {
+  myMapHandler.load();
 
   $(".burger-icon-link, .burger-info-name").click(function(e) {
-   //do something
-   e.stopPropagation();
-})
-
+    e.stopPropagation();
+  });
 
   $(".burger-panel").click(function() {
-    console.log('test')
+
     lat = $(this).find('.burger-lat')[0].value;
     long = $(this).find('.burger-long')[0].value;
-    console.log(lat, long)
-    var newLatLng = new google.maps.LatLng(parseFloat(lat), parseFloat(long))
 
-    map.panTo(newLatLng);
-    updateMarkers();
+    myMapHandler.moveTo(lat, long);
+
     return false;
   });
 
   //  Check Radio-box
-    $(".rating input:radio").attr("checked", false);
-    $('.rating input').click(function () {
-        $(".rating span").removeClass('checked');
-        $(this).parent().addClass('checked');
-    });
-
-    $('input:radio').change(
-    function(){
-        var userRating = this.value;
-        //alert(userRating);
-    });
-})
-
-
-function updateMarkers() {
-  $.getJSON('/api/nearest/'+lat+'/'+long, function(data){
-    console.log(data)
-    $.each( data, function( key, burgerInfo ) {
-
-      var pinIcon = new google.maps.MarkerImage(
-        '/static/images/burger-fallback.png',
-        null, /* size is determined at runtime */
-        null, /* origin is 0,0 */
-        null, /* anchor is bottom center of the scaled image */
-        new google.maps.Size(20, 20)
-      );
-      var marker = new google.maps.Marker({map: map, icon: pinIcon})
-      //console.log(parseFloat(burgerInfo.lat), parseFloat(burgerInfo.long))
-      //console.log(long)
-      var newLatLng = new google.maps.LatLng(parseFloat(burgerInfo.lat), parseFloat(burgerInfo.long))
-      //console.log(newLatLng)
-
-      marker.setPosition(newLatLng);
-      //marker.setLabel(burgerInfo.burgerName)
-   });
-  })
-}
-
-
-function initMiniMap() {
-  var myLatlng = {lat: lat, lng: long};
-  var mapDiv = document.getElementById('minimap');
-   map = new google.maps.Map(mapDiv, {
-    center: {lat: lat, lng: long},
-    zoom: 8
+  $(".rating input:radio").attr("checked", false);
+  $('.rating input').click(function () {
+    $(".rating span").removeClass('checked');
+    $(this).parent().addClass('checked');
   });
 
-  //var marker = new google.maps.Marker({map: map})
-  updateMarkers();
-  google.maps.event.addListener(map, 'dragend',  updateMarkers );
-  google.maps.event.addListener(map, 'zoom_changed',  updateMarkers );
-
-}
+  $('input:radio').change(
+  function(){
+    var userRating = this.value;
+    //alert(userRating);
+  });
+})
