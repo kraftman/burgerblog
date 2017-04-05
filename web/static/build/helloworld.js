a6a7e8aab30f457e26c8339ae4df5d38bca54e86
Chris Tanner <me@itschr.is>
2017-04-05 19:03:46 +0100

    add the jsx files

--- web/static/build/helloworld.js
+++ web/static/build/helloworld.js
@@ -6,21 +6,28 @@ MenuItem = ReactBootstrap.MenuItem,
 NavDropdown = ReactBootstrap.NavDropdown;
 
 const navbarInstance = (
-  React.createElement(Navbar, null, 
+  React.createElement(Navbar, {inverse: true, collapseOnSelect: true}, 
     React.createElement(Navbar.Header, null, 
       React.createElement(Navbar.Brand, null, 
         React.createElement("a", {href: "#"}, "React-Bootstrap")
-      )
+      ), 
+      React.createElement(Navbar.Toggle, null)
     ), 
-    React.createElement(Nav, null, 
-      React.createElement(NavItem, {eventKey: 1, href: "#"}, "Link"), 
-      React.createElement(NavItem, {eventKey: 2, href: "#"}, "Link"), 
-      React.createElement(NavDropdown, {eventKey: 3, title: "Dropdown", id: "basic-nav-dropdown"}, 
-        React.createElement(MenuItem, {eventKey: 3.1}, "Action"), 
-        React.createElement(MenuItem, {eventKey: 3.2}, "Another action"), 
-        React.createElement(MenuItem, {eventKey: 3.3}, "Something else here"), 
-        React.createElement(MenuItem, {divider: true}), 
-        React.createElement(MenuItem, {eventKey: 3.4}, "Separated link")
+    React.createElement(Navbar.Collapse, null, 
+      React.createElement(Nav, null, 
+        React.createElement(NavItem, {eventKey: 1, href: "#"}, "Link"), 
+        React.createElement(NavItem, {eventKey: 2, href: "#"}, "Link"), 
+        React.createElement(NavDropdown, {eventKey: 3, title: "Dropdown", id: "basic-nav-dropdown"}, 
+          React.createElement(MenuItem, {eventKey: 3.1}, "Action"), 
+          React.createElement(MenuItem, {eventKey: 3.2}, "Another action"), 
+          React.createElement(MenuItem, {eventKey: 3.3}, "Something else here"), 
+          React.createElement(MenuItem, {divider: true}), 
+          React.createElement(MenuItem, {eventKey: 3.3}, "Separated link")
+        )
+      ), 
+      React.createElement(Nav, {pullRight: true}, 
+        React.createElement(NavItem, {eventKey: 1, href: "#"}, "Link Right"), 
+        React.createElement(NavItem, {eventKey: 2, href: "#"}, "Link Right")
       )
     )
   )
