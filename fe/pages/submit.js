28d508da05b64bc17365fd0ec4a76d61f2677459
Chris Tanner <me@itschr.is>
2018-08-19 18:05:30 -0700

    basic file upload working

--- fe/pages/submit.js
+++ fe/pages/submit.js
@@ -1,50 +1,49 @@
 import Layout from '../components/MyLayout.js';
-import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
+import { post } from 'axios';
+import React from 'react';
 
-const onChange = e => {
-    this.setState({file:e.target.files[0]})
-}
+const fileUpload = (data, restaurantName) => {
+  const url = 'http://localhost/api/upload';
+  const formData = new FormData();
+  console.log(data, restaurantName);
+  formData.set('icon', data);
+  formData.set('image', data);
+  formData.set('restaurantName', restaurantName);
+  const config = {
+    headers: {
+      'content-type': 'multipart/form-data',
+    },
+  };
+  return post(url, formData, config);
+};
 
-const onFormSubmit = (e) => {
-e.preventDefault() // Stop form submit
-this.fileUpload(this.state.file).then((response)=>{
-    console.log(response.data);
-})
-}
+const getRadioGroup = (label, name) => {
+  const radioRows = [];
 
-const radioRows = []
-
-for(let i = 1; i <= 10; i++) {
-    radioRows.push(<input type="radio" key="{i}" id="{i}" label="{i}"/>)
-}
-
-const Index = (props) => (
-  <Layout {...props}>
-    <h1>Recent</h1>
-    {/* big map
-      file upload
-      Date picker
-      restaurant name
-      burger name
-      burger notes
-      meat flavour
-      meat texture
-      meat succulence
-      meat volume
-      bun rating
-      topping rating
-      side rating
-    */}
-    <form onSubmit={this.onFormSubmit}>
-        <input type="file" onChange={this.onChange} />
-        <radiogroup>
-            {radioRows}
-        </radiogroup>
-        <button type="submit">Upload</button>
-    </form>
-  </Layout>
-);
+  for (let i = 1; i <= 10; i++) {
+    radioRows.push(
+      <label htmlFor={i}>
+        <input
+          type="radio"
+          key={name + i}
+          id={name + i + 'label'}
+          label={i}
+          name={name}
+        />
+        {i}
+      </label>
+    );
+  }
+  const radioGroup = [];
+  radioGroup.push(
+    <div>
+      {label}
+      <radiogroup>{radioRows}</radiogroup>
+    </div>
+  );
+  return radioGroup;
+};
 
 const getTopBurgers = async () => {
   let topBurgers = await fetch('http://localhost/api/top/10');
@@ -52,13 +51,90 @@ const getTopBurgers = async () => {
   return topBurgers;
 };
 
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      restaurantName: 'srtrst',
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onFileChange = this.onFileChange.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  static async getInitialProps({ query }) {
+    const topBurgers = await getTopBurgers();
+    return {
+      topBurgers,
+    };
+  }
 
-Index.getInitialProps = async function({ pathname, query }) {
-  const topBurgers = await getTopBurgers();
-  console.log(query);
-  return {
-    topBurgers,
-  };
-};
+  onFileChange(e) {
+    console.log('adding file: ', e.target.files[0]);
+    this.setState({ file: e.target.files[0] });
+  }
 
-export default Index;
+  onChange(event) {
+    console.log('event:', event.target);
+    console.log('event: ', event.target.value);
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault(); // Stop form submit
+    console.log('fileupload: ', this.state.file, this.state.restaurantName);
+    return fileUpload(this.state.file, this.state.restaurantName)
+      .then((res) => {
+        console.log('got', res);
+      })
+      .catch((err) => {
+        console.log('err from post: ', err);
+      });
+  }
+
+  render() {
+    return (
+      <Layout {...this.props}>
+        <h1>Recent</h1>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="file" name="burgerPhoto" onChange={this.onFileChange} />
+          <label htmlFor="dateEaten">Date</label>
+          <input
+            type="date"
+            name="dateEaten"
+            value=""
+            onChange={this.onChange}
+          />
+
+          <label htmlFor="restaurantName">Restaurant Name</label>
+          <input
+            type="text"
+            name="restaurantName"
+            value={this.state.restaurantName || ''}
+            onChange={this.onChange}
+          />
+
+          <label htmlFor="burgerName">Burger Name</label>
+          <input type="text" name="burgerName" value="" />
+
+          <input id="lat" type="hidden" name="lat" value="" />
+          <input id="long" type="hidden" name="long" value="" />
+
+          <label htmlFor="burgerNotes">Burger Notes</label>
+          <textarea name="burgerNotes" />
+
+          <div>
+            {getRadioGroup('Meat Flavour', 'meatflavour')}
+            {getRadioGroup('Meat Texture', 'meatTexture')}
+            {getRadioGroup('Meat Succulence', 'meatSucculence')}
+            {getRadioGroup('Meat Ratio', 'meatRatio')}
+            {getRadioGroup('Bun Rating', 'bunRating')}
+            {getRadioGroup('Topping Rating', 'toppingRating')}
+            {getRadioGroup('Side Rating', 'sideRating')}
+          </div>
+          <button type="submit">Upload</button>
+        </form>
+      </Layout>
+    );
+  }
+}
--- fe/routes/api.js
+++ fe/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const burgerDal = require('../dal/burgerdal');
 const { checkSchema } = require('express-validator/check');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
 
 const validator = () => {
   return {
@@ -21,6 +23,19 @@ router.get('/bottom/:count', checkSchema(validator), async function(req, res) {
   const count = req.params.count || 10;
   const topBurgers = await burgerDal.getWorstBurgers(count);
   return res.json(topBurgers);
+});
+
+var cpUpload = upload.fields([
+  { name: 'icon', maxCount: 1 },
+  { name: 'image', maxCount: 1 },
+  { name: 'restaurantName', maxCount: 1 },
+]);
+
+router.post('/upload', cpUpload, async function(req, res) {
+  console.log(req.files['icon']);
+  console.log(req.files['image']);
+  console.log(req.body['restaurantName']);
+  res.json({ success: true });
 });
 
 module.exports = router;
